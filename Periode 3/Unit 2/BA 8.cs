487ea2c64eba2bd4e1b70c758c9c4b7b0805c642
v <39436798+thunderedge@users.noreply.github.com>
2019-06-12 02:40:19 +0200

    Update BA 8.cs

--- Periode 3/Unit 2/BA 8.cs
+++ Periode 3/Unit 2/BA 8.cs
@@ -4,4 +4,27 @@ using System.Threading;
 
 namespace U2BA8
 {
+	class Simple
+	{
+		public static Func<string, string> Then(Func<string, string> f, Func<string, string> g)
+		{
+			return x => g(f(x));
+		}
+
+		public static Func<string, string> Boom(int n, Func<string, string> f)
+		{
+			return n <= 0 ? x => x : Then(f, Boom(n -1, f));
+		}
+
+		public static Func<string, Func<string, string>> Draw = sym => s => s + sym;
+		
+		public static void Main(string[] args)
+		{
+			var star = Draw("*");
+            var hash = Draw("*");
+			Func<string, string> fig = Boom(3, Then(star,Then(hash, star)));
+			string res = fig("");
+			res = res + "!";
+		}
+	}
 }
