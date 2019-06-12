2290d1a33bdf6180f31120fe54519149d3301c7a
v <39436798+thunderedge@users.noreply.github.com>
2019-06-12 02:53:42 +0200

    Update BA 2.cs

--- Periode 3/Unit 3/BA 2.cs
+++ Periode 3/Unit 3/BA 2.cs
@@ -4,4 +4,18 @@ using System.Threading;
 
 namespace U3BA2
 {
+	class Simple
+    {
+		static void Main(string[] args)
+        {
+			int[] array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+            array[0] = 99;
+            array[9] = 88;
+        }
+    }
 }
