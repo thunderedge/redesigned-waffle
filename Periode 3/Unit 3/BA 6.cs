8ab918ca609f88d76ebb09e7bc1b8e42caf93746
v <39436798+thunderedge@users.noreply.github.com>
2019-06-12 03:29:47 +0200

    Update BA 6.cs

--- Periode 3/Unit 3/BA 6.cs
+++ Periode 3/Unit 3/BA 6.cs
@@ -4,4 +4,26 @@ using System.Threading;
 
 namespace U3BA6
 {
+  class Simple
+	{
+		static string[] push(string[] array, string newValue)
+		{
+			var newLength = array;
+			var newArr = newValue;
+			
+			for (int i =0; i < newLength.Length; i = i + 1)
+			{
+				;}
+			;
+			return newArr;
+		}
+
+		static void Main(string[] args)
+		{
+			string[] str = new string[] { "a", "b", "c" };
+			str = push(str, "d");
+			string[] str1 = new string[] { "tim", "jim", "pim", "bim", "lim" };
+			str1 = push(str1, "wim");
+		}
+	}
 }
