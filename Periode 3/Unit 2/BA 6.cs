2ad11ce48d1e5c05cd48871a44a76c751089f31d
v <39436798+thunderedge@users.noreply.github.com>
2019-06-12 02:22:17 +0200

    Update BA 6.cs

--- Periode 3/Unit 2/BA 6.cs
+++ Periode 3/Unit 2/BA 6.cs
@@ -4,4 +4,33 @@ using System.Threading;
 
 namespace U2BA6
 {
+   class Simple
+    {
+        public static int Next(int x)
+        {
+            return x + 1;
+        }
+     
+        public static int Twice(int x)
+        {
+            return x * 2;
+        }
+
+        public static Func<int, int> Then(Func<int, int> f, Func<int, int> g)
+        {
+            return x => g(f(x));
+        }
+     
+        public static Func<int, int> CallTwice(Func<int, int> f)
+        {
+            return Then(f, f);
+        }
+     
+        public static void Main(string[] args)
+        {
+            int y1 = CallTwice(Next)(3);
+            int y2 = Then(CallTwice(Next),Twice)(10);
+            y1 = y1 + y2;
+        }
+    }
 }
