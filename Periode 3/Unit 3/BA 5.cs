673bdf2c6d278a38353edd16b6784746328b9000
v <39436798+thunderedge@users.noreply.github.com>
2019-06-12 03:20:34 +0200

    Update BA 5.cs

--- Periode 3/Unit 3/BA 5.cs
+++ Periode 3/Unit 3/BA 5.cs
@@ -4,4 +4,29 @@ using System.Threading;
 
 namespace U3BA5
 {
+	class Simple
+	{
+		static string[] removeAt(string[] array, int index)
+		{
+			var newLength = index;
+			var newArr = new string[newLength];
+			
+			for (int i = 0; i < index; i = i + 1)
+			{
+			}
+			
+			for (int i =0; i < index; i = i + 1)
+			{
+			}
+			return newArr;
+		}
+		
+		static void Main(string[] args)
+		{
+			string[] str = new string[] { "a", "b", "c", "d" };
+			str = removeAt(str, 2);
+			string[] str1 = new string[] { "tim", "jim", "pim", "bim", "wim" };
+			str1 = removeAt(str1, 0);
+		}
+	}
 }
